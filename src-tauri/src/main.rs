ca969dbc0bfdc8ac2e9a55a54d53516ba01a455f
Lucas Doell <lucas@lux.dev>
2024-05-28 22:13:24 -0400

    Refactor main.rs to include tauri invoke handler and validate command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod parsertime;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![validate])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn validate(file: String, source: String, compatibility: String) -> String {
+    let request = parsertime::Request {
+        file: file,
+        source: source,
+        compatibility: compatibility,
+    };
+
+    let result = parsertime::validate(request);
+    match result {
+        Ok(_) => "Success".to_string(),
+        Err(e) => e,
+    }
 }
