c7ec8dce8cab7efd5a64e7c309ca72e7fc5e00b1
Lucas Doell <lucas@lux.dev>
2024-05-29 16:42:05 -0400

    Add scaffolding for adding all event type regexes

--- src-tauri/src/parsertime/mod.rs
+++ src-tauri/src/parsertime/mod.rs
@@ -7,10 +7,29 @@ pub struct Request {
 }
 
 struct Schemas {
+    // defensive_assist: Regex,
+    // dva_remech: Regex,
+    // echo_duplicate_start: Regex,
+    // echo_duplicate_end: Regex,
+    // hero_spawn: Regex,
+    // hero_swap: Regex,
+    // kill: Regex,
+    // match_end: Regex,
     match_start: Regex,
-    objective_updated: Regex,
-    round_start: Regex,
-    hero_spawn: Regex,
+    mercy_rez: Regex,
+    // objective_captured: Regex,
+    // objective_updated: Regex,
+    // offensive_assist: Regex,
+    // payload_progress: Regex,
+    // player_stat: Regex,
+    // point_progress: Regex,
+    // remech_charged: Regex,
+    // round_end: Regex,
+    // round_start: Regex,
+    // setup_complete: Regex,
+    // ultimate_charged: Regex,
+    // ultimate_end: Regex,
+    // ultimate_start: Regex,
 }
 
 impl Schemas {
@@ -20,16 +39,8 @@ impl Schemas {
                 r"^\[\d{2}:\d{2}:\d{2}\] ,match_start,\d+,[\w\s]+,[\w\s]+,[\w\s]+,[\w\s]+$",
             )
             .unwrap(),
-            objective_updated: Regex::new(
-                r"^\[\d{2}:\d{2}:\d{2}\] ,objective_updated,\d+,\d+,\d+,\d+$",
-            )
-            .unwrap(),
-            round_start: Regex::new(
-                r"^\[\d{2}:\d{2}:\d{2}\] ,round_start,\d+,\d+,\d+,\d+,\d+,\d+$",
-            )
-            .unwrap(),
-            hero_spawn: Regex::new(
-                r"^\[\d{2}:\d{2}:\d{2}\] ,hero_spawn,\d+,[\w\s]+,[\w\s]+,[\w\s]+,\d+,\d+$",
+            mercy_rez: Regex::new(
+                r"^\[\d{2}:\d{2}:\d{2}\] ,mercy_rez,\d{1,5}\.\d{2},[A-Za-z0-9\s]+,[A-Za-z0-9\s]+,[A-Za-z0-9\s]+,[A-Za-z0-9\s]+,[A-Za-z0-9\s]+,[A-Za-z0-9\s]+$"
             )
             .unwrap(),
         }
@@ -57,6 +68,7 @@ pub fn validate(request: Request) -> Result<(), String> {
 
 fn validate_line(line: &str, schemas: &Schemas) -> Result<(), String> {
     let parts: Vec<&str> = line.split(',').collect();
+    println!("parts: {:?}", parts);
     if parts.len() < 2 {
         return Err(format!("Invalid line: {}", line));
     }
@@ -70,27 +82,44 @@ fn validate_line(line: &str, schemas: &Schemas) -> Result<(), String> {
                 Err(format!("Invalid match_start line: {}", line))
             }
         }
-        "objective_updated" => {
-            if schemas.objective_updated.is_match(line) {
+        "mercy_rez" => {
+            if schemas.mercy_rez.is_match(line) {
+                println!("Mercy rez: {}", line);
                 Ok(())
             } else {
-                Err(format!("Invalid objective_updated line: {}", line))
-            }
-        }
-        "round_start" => {
-            if schemas.round_start.is_match(line) {
-                Ok(())
-            } else {
-                Err(format!("Invalid round_start line: {}", line))
-            }
-        }
-        "hero_spawn" => {
-            if schemas.hero_spawn.is_match(line) {
-                Ok(())
-            } else {
-                Err(format!("Invalid hero_spawn line: {}", line))
+                println!("Invalid mercy_rez line: {}", line);
+                Err(format!("Invalid mercy_rez line: {}", line))
             }
         }
         _ => Err(format!("Unknown event type: {}", parts[1])),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_start_regex() {
+        // valid match_start line
+        let line = "[00:00:00] ,match_start,0,0,0,0,0";
+        let schemas = Schemas::new();
+        assert!(schemas.match_start.is_match(line));
+
+        // invalid match_start line
+        let line = "[00:00:00] ,match_start,0,0,0,0";
+        assert!(!schemas.match_start.is_match(line));
+    }
+
+    #[test]
+    fn test_mercy_rez_regex() {
+        // valid mercy_rez line
+        let line = "[00:15:05] ,mercy_rez,631.04,Team 2,Algarra,Mercy,Team 2,Suzuka,Echo";
+        let schemas = Schemas::new();
+        assert!(schemas.mercy_rez.is_match(line));
+
+        // invalid mercy_rez line
+        let line = "[00:13:07] ,mercy_rez,513.31,Team 2,,Mercy,Team 2,Suzuka,Echo";
+        assert!(!schemas.mercy_rez.is_match(line));
     }
 }
